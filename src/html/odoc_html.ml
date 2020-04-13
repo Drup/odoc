a6924a8f350606249531e4acb7ea2588bd3b8921
Drup <drupyog@zoho.com>
2020-04-13 17:53:59 +0200

    Remove Odoc_html.Url for good.

--- src/html/odoc_html.ml
+++ src/html/odoc_html.ml
@@ -1,10 +1,3 @@
 module Tree = Tree
 module Targets = Targets
-
 module Generator = Generator
-
-(* Exposed as an unstable public API for third-party packages to "hack" on, see
-
-    https://github.com/ocaml/odoc/pull/252
-    https://github.com/ocaml/odoc/issues/236. *)
-module Url = Url
