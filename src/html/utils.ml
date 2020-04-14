95fca03ad298a71c528ca257bffdcf50e29c5294
Drup <drupyog@zoho.com>
2020-04-14 17:33:51 +0200

    Avoid recent OCaml features.

--- src/html/utils.ml
+++ src/html/utils.ml
@@ -2,6 +2,11 @@ module Html = Tyxml.Html
 
 (* Shared utility functions *)
 
+(* = Option.fold *)
+let fold_option ~none ~some = function
+  | Some x -> some x
+  | None -> none
+
 let rec list_concat_map ?sep ~f = function
   | [] -> []
   | [x] -> f x
